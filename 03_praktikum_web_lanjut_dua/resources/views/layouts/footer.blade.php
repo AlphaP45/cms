e199396bd0cedfc6e31def0d294978b76e87e51a
AlphaP45 <alphapramadhani45@gmail.com>
2022-03-03 20:49:36 +0700

    first commit

---
+++ 03_praktikum_web_lanjut_dua/resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="text-center">
+            <p class="small copyright-text">Copyright &copy; Kelompok 3 - TI2E</p>
+</footer>
---
+++ 03_praktikum_web_lanjut_satu/routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
+Auth::routes();
+
+// Halaman Home : route biasa
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', [WelcomeController::class, 'welcome']);
+
+// Halaman Product : route prefix
+Route::prefix('category')->group(function () {
+    Route::get('/marbel-edu-games', [ProductController::class, 'edugames']);
+    Route::get('/marbel-and-friends-kids-games', [ProductController::class, 'kidsgames']);
+    Route::get('/riri-story-books', [ProductController::class, 'storybooks']);
+    Route::get('/kolak-kids-songs', [ProductController::class, 'kidsongs']);
+});
+
+// Halaman News : route params
+Route::get('news/{slug?}', [NewsController::class, 'show'])->name('news');
+
+// Halaman Program : route prefix
+Route::prefix('program')->group(function () {
+    Route::get('/karir', [ProgramController::class, 'karir']);
+    Route::get('/magang', [ProgramController::class, 'magang']);
+    Route::get('/kunjungan-industri', [ProgramController::class, 'kunjunganIndustri']);
+});
+
+// Halaman About-Us : route biasa
+Route::get('about-us', [AboutUsController::class, 'index'])->name('about');
+
+// Halaman Contact-Us : route resource
+Route::resource('contact-us', ContactController::class)->only([
+    'index', 'store'
+]);
