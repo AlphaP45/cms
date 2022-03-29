3bc542b8f8c0318de327be22dee90f00ca832fd0
AlphaP45 <alphapramadhani45@gmail.com>
2022-03-29 11:51:34 +0700

    Prak Authentication

--- 06_praktikum_web_lanjut_Authentication/database/migrations/2022_03_29_035101_tambah_kolom_di_table_user.php
+++ 06_praktikum_web_lanjut_Authentication/database/migrations/2022_03_29_035101_tambah_kolom_di_table_user.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePelanggansTable extends Migration
+class TambahKolomDiTableUser extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreatePelanggansTable extends Migration
      */
     public function up()
     {
-        Schema::create('pelanggans', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        //
+        Schema::table('users', function(Blueprint $table){
+            $table->string('username', 20)->after('id')->nullable()->unique();
         });
     }
 
@@ -26,6 +26,9 @@ class CreatePelanggansTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pelanggans');
+        //
+        Schema::table('users', function(Blueprint $table){
+            $table->dropColumn('username');
+        });
     }
 }
